6801f2d9878487f2663d70c1f1646e6a0e90d257
VAI Projects <vai_projects@visualartists.ie>
2024-11-06 14:51:21 +0000

    New Start

--- clock.js
+++ clock.js
@@ -38,47 +38,5 @@ function showTime() {
     ).innerHTML = currentTime;
 }
  
-showTime();
 
-//Drawing Room
-//10.00AM-11.40AM
-const body = document.querySelector('.overlay-text-drawing-1');
-const date = new Date();
-const hour = date.getHours();
-const min = date.getMinutes();
 
-if (hour < 10.00 ) {
-  body.style.color = 'white';
-} else if (hour >= 11.40) {
-  body.style.color = 'white';
-} else {
-  body.style.color = 'yellow';
-}
-
-//11.40AM-12.00AM
-const body1 = document.querySelector('.overlay-text-drawing-2');
-const date1 = new Date();
-const hour1 = date.getHours();
-const min1 = date.getMinutes();
-
-if (hour <= 11.40) {
-  body1.style.color = 'white';
-} else if (hour >= 12.00) {
-  body1.style.color = 'white';
-} else {
- body1.style.color = 'blue';
-}
-
-//12.00PM-1.00PM
-const body2 = document.querySelector('.overlay-text-drawing-3');
-const date2 = new Date();
-const hour2 = date.getHours();
-const min2 = date.getMinutes();
-
-if (hour <= 12.00) {
-  body2.style.color = 'white';
-} else if (hour >= 13.00) {
-  body2.style.color = 'white';
-} else {
- body2.style.color = 'green';
-}
