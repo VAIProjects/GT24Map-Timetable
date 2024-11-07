2544d5c22b3fada19d090da81e7776e34084c18a
VAI Projects <vai_projects@visualartists.ie>
2024-11-07 16:03:17 +0000

    added new links

--- javascript/timetable1.js
+++ javascript/timetable1.js
@@ -2,6 +2,6 @@ const now = new Date();
 const expires = new Date('2024-11-07 10:30:00');
 const delay = expires - now;
 window.setTimeout(function() {
-    window.location = 'https://8000-vaiprojects-gt24maptime-lopag3wydxm.ws-eu116.gitpod.io/html%20pages/timetable2.html';
+    window.location = 'https://vaiprojects.github.io/GT24Map-Timetable/html%20pages/timetable2.html';
 }, delay);
 
