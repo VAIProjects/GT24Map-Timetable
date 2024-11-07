63e643bcf963e1d90cbc1e8e6c0749ce240d82a1
VAI Projects <vai_projects@visualartists.ie>
2024-11-07 13:00:34 +0000

    Updated all slides, just images needing to be done

---
+++ javascript/timetable1.js
@@ -0,0 +1,7 @@
+const now = new Date();
+const expires = new Date('2024-11-07 10:30:00');
+const delay = expires - now;
+window.setTimeout(function() {
+    window.location = 'https://8000-vaiprojects-gt24maptime-a5y7puhuqfp.ws-eu116.gitpod.io/html%20pages/timetable2.html';
+}, delay);
+
---
+++ javascript/timetable3.js
@@ -0,0 +1,6 @@
+const now = new Date();
+const expires = new Date('2024-11-07 12:00:00');
+const delay = expires - now;
+window.setTimeout(function() {
+    window.location = 'https://8000-vaiprojects-gt24maptime-a5y7puhuqfp.ws-eu116.gitpod.io/html%20pages/timetable4.html';
+}, delay);
