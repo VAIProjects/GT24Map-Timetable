f03044a2bd5ec353a1f7f10ed5b2521a4255e6c7
VAI Projects <vai_projects@visualartists.ie>
2024-11-08 11:17:02 +0000

    minor updates

--- javascript/timetable5.js
+++ javascript/timetable5.js
@@ -2,5 +2,7 @@ const now = new Date();
 const expires = new Date('2024-11-07 14:00:00');
 const delay = expires - now;
 window.setTimeout(function() {
-    window.location = 'https://vaiprojects.github.io/gettogethermap/htmlpages/timetable6.html';
+    window.location.href = "htmlpages/timetable2";
 }, delay);
+
+setTimeout(function(){window.location.href='http://yourdomainname/views/add.html'},1000);
